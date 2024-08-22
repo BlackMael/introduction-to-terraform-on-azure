7b3b19828d070d81ac8091b5ffbc8ec0fa2c5218
Martin Harris <github@blackmael.com>
2024-08-22 18:46:42 +1200

    Add NIC creation in Australia East Create a network interface card (NIC) in Australia East region to connect to subnet for dynamic IP allocation.

--- main.tf
+++ main.tf
@@ -37,3 +37,17 @@ resource "azurerm_subnet" "main" {
 
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+# Create network interface card (NIC)
+resource "azurerm_network_interface" "internal" {
+  name                = "learn-tf-nic-int-australiaeast"
+ 
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
