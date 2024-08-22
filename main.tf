00a49d3f4856bcbf8fce2f63486b7e5221885f7b
Martin Harris <github@blackmael.com>
2024-08-22 18:42:41 +1200

    Add virtual network subnet creation in main.tf
    Added code to create a subnet within the virtual network in main.tf. This includes defining the subnet name, address prefixes, and associating it with the existing resources.

--- main.tf
+++ main.tf
@@ -21,7 +21,19 @@ resource "azurerm_resource_group" "main" {
 # Create virtual network
 resource "azurerm_virtual_network" "main" {
   name                = "learn-tf-vnet-australiaeast"
+
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+
   address_space       = ["10.0.0.0/16"]
+}
+
+# Create subnet
+resource "azurerm_subnet" "main" {
+  name                 = "learn-tf-subnet-australiaeast"
+
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes     = ["10.0.0.0/24"]
 }
